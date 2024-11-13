9bd134111fdeeca88d73c6d6dd4a4c3b8782aa2d
Gleb <Gleb@DESKTOP-4EM8SV3>
2024-11-13 13:27:16 +0200

    Add project files.

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelResult = new System.Windows.Forms.Label();
+            this.labelCorrect = new System.Windows.Forms.Label();
+            this.labelIncorrect = new System.Windows.Forms.Label();
+            this.labelIncorrectQuestions = new System.Windows.Forms.Label();
+            this.btnGradeExam = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.Location = new System.Drawing.Point(28, 27);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(45, 16);
+            this.labelResult.TabIndex = 0;
+            this.labelResult.Text = "Result";
+            this.labelResult.Click += new System.EventHandler(this.labelResult_Click);
+            // 
+            // labelCorrect
+            // 
+            this.labelCorrect.AutoSize = true;
+            this.labelCorrect.Location = new System.Drawing.Point(28, 99);
+            this.labelCorrect.Name = "labelCorrect";
+            this.labelCorrect.Size = new System.Drawing.Size(50, 16);
+            this.labelCorrect.TabIndex = 1;
+            this.labelCorrect.Text = "Correct";
+            this.labelCorrect.Click += new System.EventHandler(this.labelCorrect_Click);
+            // 
+            // labelIncorrect
+            // 
+            this.labelIncorrect.AutoSize = true;
+            this.labelIncorrect.Location = new System.Drawing.Point(28, 190);
+            this.labelIncorrect.Name = "labelIncorrect";
+            this.labelIncorrect.Size = new System.Drawing.Size(58, 16);
+            this.labelIncorrect.TabIndex = 2;
+            this.labelIncorrect.Text = "Incorrect";
+            // 
+            // labelIncorrectQuestions
+            // 
+            this.labelIncorrectQuestions.AutoSize = true;
+            this.labelIncorrectQuestions.Location = new System.Drawing.Point(28, 277);
+            this.labelIncorrectQuestions.Name = "labelIncorrectQuestions";
+            this.labelIncorrectQuestions.Size = new System.Drawing.Size(121, 16);
+            this.labelIncorrectQuestions.TabIndex = 3;
+            this.labelIncorrectQuestions.Text = "Incorrect Questions";
+            // 
+            // btnGradeExam
+            // 
+            this.btnGradeExam.Location = new System.Drawing.Point(31, 344);
+            this.btnGradeExam.Name = "btnGradeExam";
+            this.btnGradeExam.Size = new System.Drawing.Size(75, 23);
+            this.btnGradeExam.TabIndex = 4;
+            this.btnGradeExam.Text = "GradeExam";
+            this.btnGradeExam.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnGradeExam);
+            this.Controls.Add(this.labelIncorrectQuestions);
+            this.Controls.Add(this.labelIncorrect);
+            this.Controls.Add(this.labelCorrect);
+            this.Controls.Add(this.labelResult);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelResult;
+        private System.Windows.Forms.Label labelCorrect;
+        private System.Windows.Forms.Label labelIncorrect;
+        private System.Windows.Forms.Label labelIncorrectQuestions;
+        private System.Windows.Forms.Button btnGradeExam;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public readonly char[] correctAnswers = {
+            'B', 'D', 'A', 'A', 'C', 'A', 'B', 'A', 'C', 'D',
+            'B', 'C', 'D', 'A', 'D', 'C', 'C', 'B', 'D', 'B'
+        };
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Load += new System.EventHandler(this.Form1_Load);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Welcome to the Driver's License Exam Application!");
+        }
+
+        private void btnGradeExam_Click(object sender, EventArgs e)
+        {
+            string filePath = @"answers.txt";
+
+            if (!File.Exists(filePath))
+            {
+                MessageBox.Show("File not found: " + filePath);
+                return;
+            }
+
+            string[] studentAnswers;
+            try
+            {
+                studentAnswers = File.ReadAllLines(filePath);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error reading the file: " + ex.Message);
+                return;
+            }
+            if (studentAnswers.Length != correctAnswers.Length)
+            {
+                MessageBox.Show("The file does not contain the correct number of answers.");
+                return;
+            }
+
+            int correctCount = 0;
+            List<int> incorrectQuestions = new List<int>();
+
+            for (int i = 0; i < correctAnswers.Length; i++)
+            {
+                if (studentAnswers[i][0] == correctAnswers[i])
+                {
+                    correctCount++;
+                }
+                else
+                {
+                    incorrectQuestions.Add(i + 1);
+                }
+            }
+
+            int incorrectCount = correctAnswers.Length - correctCount;
+            bool passed = correctCount >= 15;
+
+            labelResult.Text = passed ? "Result: Passed" : "Result: Failed";
+            labelCorrect.Text = $"Correct Answers: {correctCount}";
+            labelIncorrect.Text = $"Incorrect Answers: {incorrectCount}";
+            labelIncorrectQuestions.Text = "Incorrect Questions: " + string.Join(", ", incorrectQuestions);
+        }
+
+        private void labelResult_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labelCorrect_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
